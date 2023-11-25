84cb1143cc33ec5ab07cee048da46daf44830735
Ilkin Hamzayev <ilkinhamzayev925@gmail.com>
2023-11-25 20:53:39 +0100

    Rewritten the logic

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const gridContainer = document.getElementById('gridContainer');
 
-function createGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+
+function createGrid(){
+    for(let i = 0; i < 24*24; i++ ){
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         gridContainer.appendChild(gridItem);
-    }
-}
 
+        // Adding the event listener
+        gridItem.addEventListener('mouseover', function(){
+            gridItem.style.backgroundColor = 'grey';
+        })
+        //Reset the color 
+        gridItem.addEventListener('mouseout',function(){
+            gridItem.style.backgroundColor = '';
+        })
+
+    }
+}    
 createGrid();
